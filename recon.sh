4c1e9f077093bab1e60515fdbc98935a78339296
kyretof <106271210+kyretof@users.noreply.github.com>
2022-06-17 11:52:48 +0800

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -14,28 +14,28 @@ fi
 
 echo “[+] Harvesting subdomains with amass...”
 amass enum -d $domain | anew $url/a.txt
-sort -u $dir/a.txt | anew $dir/amass-final.txt
+sort -u $dir/a.txt | anew $dir/amass.txt
 rm $dir/a.txt
 
 echo “[+] Harvesting subdomains with assetfinder...”
 assetfinder --subs-only $domain | anew $dir/a.txt
-cat $dir/a.txt | anew $dir/assetfinder-final.txt
+cat $dir/a.txt | anew $dir/assetfinder.txt
 rm $dir/a.txt
 
 echo “[+] Harvesting subdomains with subfinder...”
 subfinder -d $domain | anew $dir/a.txt
-sort -u $dir/a.txt | anew $dir/subfinder-final.txt
+sort -u $dir/a.txt | anew $dir/subfinderl.txt
 rm $dir/a.txt
 
 echo “[+] Harvesting subdomains with waybackurls...”
 echo $domain | waybackurls | anew $dir/a.txt
-sort -u $dir/a.txt | anew $dir/waybackurlstxt
+sort -u $dir/a.txt | anew $dir/waybackurls.txt
 rm $dir/a.txt
 
 echo "[+]Sorting subdomains..."
 sort -u $dir/amass.txt $dir/assetfinder.txt $dir/subfinder.txt > $dir/final-sd.txt
 
-rm $dir/amass.txt $dir/assetfinder.txt $dir/subfinder.txt
+#rm $dir/amass.txt $dir/assetfinder.txt $dir/subfinder.txt
 
 echo "[+]HTTPX..."
 httpx -silent -status-code -title -fc 404 -list $dir/subfinder.txt | anew $dir/live-sd.txt
