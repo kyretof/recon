1b81a0f0b1e5e4f95c7fc0d3fd10ae75120226a5
kyretof <106271210+kyretof@users.noreply.github.com>
2022-06-18 08:47:47 +0800

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -12,30 +12,35 @@ if [ ! -d "$dir" ]; then
 	mkdir $dir
 fi
 
-echo “[+] Harvesting subdomains with amass...”
+echo "[+] Harvesting subdomains with amass..."
 amass enum -d $domain | anew $url/a.txt
 sort -u $dir/a.txt | anew $dir/amass.txt
 rm $dir/a.txt
 
-echo “[+] Harvesting subdomains with assetfinder...”
+echo "[+] Harvesting subdomains with assetfinder..."
 assetfinder --subs-only $domain | anew $dir/a.txt
-cat $dir/a.txt | anew $dir/assetfinder.txt
+sort -u $dir/a.txt | anew $dir/assetfinder.txt
 rm $dir/a.txt
 
-echo “[+] Harvesting subdomains with subfinder...”
+echo "[+] Harvesting subdomains with subfinder..."
 subfinder -d $domain | anew $dir/a.txt
 sort -u $dir/a.txt | anew $dir/subfinderl.txt
 rm $dir/a.txt
 
-echo “[+] Harvesting subdomains with waybackurls...”
+echo "[+] Harvesting subdomains with Sublist3r..."
+python3 Sublist3r/sublist3r.py -d $domain -o $dir/a.txt
+sort -u $dir/a.txt | anew $dir/sublist3r.txt
+rm $dir/a.txt
+
+echo "[+] Harvesting subdomains with waybackurls..."
 echo $domain | waybackurls | anew $dir/a.txt
 sort -u $dir/a.txt | anew $dir/waybackurls.txt
 rm $dir/a.txt
 
 echo "[+]Sorting subdomains..."
-sort -u $dir/amass.txt $dir/assetfinder.txt $dir/subfinder.txt > $dir/final-sd.txt
+sort -u $dir/amass.txt $dir/assetfinder.txt $dir/subfinder.txt $dir/sublist3r.txt > $dir/final-sd.txt
 
-rm $dir/amass.txt $dir/assetfinder.txt $dir/subfinder.txt
+rm $dir/amass.txt $dir/assetfinder.txt $dir/subfinder.txt $dir/sublist3r.txt
 
 echo "[+]HTTPX..."
 httpx -silent -sc -fc 404 -title -list $dir/final-sd.txt | anew $dir/live-sd.txt
