84b1c3a0091572779e7b30b95e9c21a0c6ed6c8f
kyretof <kyretof@protonmail.com>
2022-06-20 01:19:51 +0000

    update recon.sh

--- recon.sh
+++ recon.sh
@@ -12,19 +12,19 @@ if [ ! -d "$dir" ]; then
 	mkdir $dir
 fi
 
-echo "[+] Harvesting subdomains with amass..."
-amass enum -d $domain | anew $url/a.txt
+echo “[+] Harvesting subdomains with amass...”
+amass enum -brute -d $domain -active -p 80,443,8000,8080,8443 | anew $dir/a.txt
 sort -u $dir/a.txt | anew $dir/amass.txt
 rm $dir/a.txt
 
-echo "[+] Harvesting subdomains with assetfinder..."
+echo “[+] Harvesting subdomains with assetfinder...”
 assetfinder --subs-only $domain | anew $dir/a.txt
 sort -u $dir/a.txt | anew $dir/assetfinder.txt
 rm $dir/a.txt
 
-echo "[+] Harvesting subdomains with subfinder..."
+echo “[+] Harvesting subdomains with subfinder...”
 subfinder -d $domain | anew $dir/a.txt
-sort -u $dir/a.txt | anew $dir/subfinderl.txt
+sort -u $dir/a.txt | anew $dir/subfinder.txt
 rm $dir/a.txt
 
 echo "[+] Harvesting subdomains with Sublist3r..."
@@ -32,7 +32,7 @@ python3 Sublist3r/sublist3r.py -d $domain -o $dir/a.txt
 sort -u $dir/a.txt | anew $dir/sublist3r.txt
 rm $dir/a.txt
 
-echo "[+] Harvesting subdomains with waybackurls..."
+echo “[+] Harvesting subdomains with waybackurls...”
 echo $domain | waybackurls | anew $dir/a.txt
 sort -u $dir/a.txt | anew $dir/waybackurls.txt
 rm $dir/a.txt
@@ -43,7 +43,7 @@ sort -u $dir/amass.txt $dir/assetfinder.txt $dir/subfinder.txt $dir/sublist3r.tx
 rm $dir/amass.txt $dir/assetfinder.txt $dir/subfinder.txt $dir/sublist3r.txt
 
 echo "[+]HTTPX..."
-httpx -silent -sc -fc 404 -title -list $dir/final-sd.txt | anew $dir/live-sd.txt
-httpx -silent -sc -fc 404 -title -list $dir/waybackurls.txt | anew $dir/live-waybackurls.txt
+httpx -silent -status-code -title -fc 404 -list $dir/final-sd.txt | anew $dir/live-sd.txt
+httpx -silent -status-code -title -fc 404 -list $dir/waybackurls.txt | anew $dir/live-waybackurls.txt
 
 rm $dir/waybackurls.txt
